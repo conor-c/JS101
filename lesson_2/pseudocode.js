cca03afca01ea7691862fe54296a392f73bdd61d
Conor Chung <ConorChung@gmail.com>
2020-12-20 10:49:05 -0800

    pseudocode practice

---
+++ lesson_2/pseudocode.js
@@ -0,0 +1,35 @@
+
+//++ a function that returns the sum of two numbers ++
+// get the first number to be added
+// get the second number to be added
+// add the two numbers together
+// return the result
+
+// ++ formal
+// START
+// GET the first number
+// --SET first number to variable 1
+// GET the second number
+// --SET second number to variable 2
+// Add variable 1 and variable 2 together
+// PRINT output to user
+// END
+
+
+// a function that takes an array of strings, 
+// and returns a string that is all those strings concatenated together
+//  get the array of strings, pass it as a parameter
+//  set a variable that we will use to hold the concatenated strings
+//  iterate over the array, adding each string to the end of the previous
+//  QUESTION: how do we want to concatenate the strings together?
+//    with a space inbetween, commas?
+//  after finishing passing over each array index, we return the concatenated variable
+
+//a function that takes an array of integers, 
+//and returns a new array with every other element
+//  receive the array of integers
+//  set an empty array variable
+//  iterate over each element in the array
+//    if the index is divisble by 2, skip to the next element
+//    is the index of the element is odd, assign it to the new array variable
+//  return the resulting array
