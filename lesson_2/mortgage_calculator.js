41f02e8f452a122e472365c02cfe6ac02b3a0a51
Conor Chung <ConorChung@gmail.com>
2021-01-04 21:25:27 -0800

    refactored and partially sanitized inputs for loan amount and length, added more output information

--- lesson_2/mortgage_calculator.js
+++ lesson_2/mortgage_calculator.js
@@ -1,32 +1,13 @@
-/*
-To get from user:
-1. The loan amount
-2. The annual percentage rate (APR) (convert to monthly for forumla use)
-3. The loan duration (convert to months)
-
-
-    m = monthlyPayment
-    p = loanAmount
-    j = monthlyInterestRate (in months)
-    n = loanDuration (in months)
-
-
-DONE return in dollar and cents amount
-
-CALCULATE: payment due every month
-total paid over the course of the loan and number of payments (which is just the loan duration in months)
-total of interest paid
-percent of interest vs percent of principle from total paid over course of the loan
-*/
 const readline = require('readline-sync');
 
 function monthlyPayment(loanAmount, monthlyInterestRate, loanDuration) {
   let monthlyInstallments = loanAmount * (monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), (-loanDuration))));
   let totalInterestPayments = (monthlyInstallments * loanDuration) - loanAmount;
+  let totalPayment = (monthlyInstallments * loanDuration).toFixed(2);
   monthlyInstallments = Number(monthlyInstallments).toFixed(2);
   totalInterestPayments = Number(totalInterestPayments).toFixed(2);
 
-  return [monthlyInstallments, totalInterestPayments];
+  return [monthlyInstallments, totalInterestPayments, totalPayment];
 }
 
 function prompt(message) {
@@ -35,25 +16,32 @@ function prompt(message) {
 
 while (true) {
   prompt('What is your loan amount?');
-  let loanAmount = readline.question();
-  loanAmount = parseFloat(loanAmount);
-
+  let loanAmount = readline.question().split(',');
+  loanAmount = parseFloat(loanAmount.join(''));
 
   prompt('What is your APR?');
-  let monthlyInterestRate = readline.question();
-  monthlyInterestRate = (parseFloat(monthlyInterestRate) / 100) / 12;
+  let monthlyInterestRate = (parseFloat(readline.question()) / 100) / 12;
 
-
-  prompt('What is your loan duration?'); // Could ask for years and months seperately
-  let loanDuration = readline.question();
-  loanDuration = Number(loanDuration);
+  prompt('How many years is your loan for?');
+  let loanYears = Number(readline.question()) * 12;
+  prompt('How many months is your loan for?');
+  let loanMonths = Number(readline.question());
+  let loanDuration = loanYears + loanMonths;
 
   let results = monthlyPayment(loanAmount, monthlyInterestRate, loanDuration);
-  prompt('your monthly payments are: $' + results[0]);
-  prompt('your total interest is: $' + results[1]);
+  prompt('Your monthly payments will be: $' + results[0]);
+  prompt('Your total paid over ' + loanDuration + ' months: $' + results[2]);
+  prompt('Your total interest is: $' + results[1]);
   prompt('Would you like to calculate another loan? Y/N');
 
   let restart = readline.question().toUpperCase();
   if (restart !== 'Y') break;
 }
 
+//THINGS TO DO
+//check if inputs from user is valid
+//specify that months input should not include years
+//make sure user can't enter in a fraction for a year .toInt?
+//internationalize text with JSON file
+//right now APR is expected in 3.2 and not .032, check if decimal is leading
+//--deal with that edge case, perhaps functionalize a validation checker?
