ef48c6f596c37162c96e7f3e8df6c64f41906575
Conor Chung <ConorChung@gmail.com>
2021-01-16 19:28:35 -0800

    implemented advice from code review

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,7 +1,4 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
-let playerScore = 0;
-let computerScore = 0;
 const WINNING_SCORE = 5;
 const WINNING_MOVES = {
   rock: ['scissors', 'lizard'],
@@ -10,14 +7,9 @@ const WINNING_MOVES = {
   lizard: ['spock', 'paper'],
   spock: ['scissors', 'rock']
 };
-
-const LOSING_MOVES = {
-  rock: ['paper', 'spock'],
-  paper: ['scissors', 'lizard'],
-  scissors: ['rock', 'spock'],
-  lizard: ['scissors', 'rock'],
-  spock: ['paper', 'lizard']
-};
+const VALID_CHOICES = Object.keys(WINNING_MOVES);
+let playerScore = 0;
+let computerScore = 0;
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -26,7 +18,7 @@ function prompt(message) {
 function calculateScore(choice, computerChoice) {
   if (WINNING_MOVES[choice].includes(computerChoice)) {
     playerScore += 1;
-  } else if (LOSING_MOVES[choice].includes(computerChoice)) {
+  } else if (WINNING_MOVES[computerChoice].includes(choice)) {
     computerScore += 1;
   }
 }
@@ -36,39 +28,50 @@ function displayWinner(choice, computerChoice) {
 
   if (WINNING_MOVES[choice].includes(computerChoice)) {
     prompt(`You win! You've won ${playerScore}/${WINNING_SCORE} times!`);
-  } else if (LOSING_MOVES[choice].includes(computerChoice)) {
+  } else if (WINNING_MOVES[computerChoice].includes(choice)) {
     prompt(`Computer Wins! Computer has won ${computerScore}/${WINNING_SCORE} times!`);
   } else {
     prompt("It's a Tie!");
   }
 }
 
-function predictChoice(choice) {
-  if (choice.startsWith(VALID_CHOICES[0].slice(0, 1))) {
-    choice = VALID_CHOICES[0];
-  } else if (choice.startsWith(VALID_CHOICES[1].slice(0, 1))) {
-    choice = VALID_CHOICES[1];
-  } else if (choice.startsWith(VALID_CHOICES[2].slice(0, 2))) {
-    choice = VALID_CHOICES[2];
-  } else if (choice.startsWith(VALID_CHOICES[3].slice(0, 1))) {
-    choice = VALID_CHOICES[3];
-  } else if (choice.startsWith(VALID_CHOICES[4].slice(0, 2))) {
-    choice = VALID_CHOICES[4];
+function autoFillChoice(choice) {
+  VALID_CHOICES.forEach(validChoice => {
+    if (validChoice.startsWith(choice) && choice !== '') {
+      choice = validChoice;
+    }
+  });
+  return choice;
+}
+
+function spockOrScissorsCheck (choice) {
+  while (choice === 's') {
+    prompt('Did you mean "sp" for Spock? Or "sc" for Scissors? Or something else?');
+    choice = readline.question().toLowerCase().trimStart();
   }
   return choice;
 }
 
 function getChoice() {
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let choice = readline.question().toLowerCase();
-  choice = predictChoice(choice);
+  let choice = readline.question().toLowerCase().trimStart();
+  choice = spockOrScissorsCheck(choice);
+  choice = autoFillChoice(choice);
 
   while (!VALID_CHOICES.includes(choice)) {
     prompt("That's not a valid choice.");
     choice = readline.question().toLowerCase();
-    choice = predictChoice(choice);
+    choice = spockOrScissorsCheck(choice);
+    choice = autoFillChoice(choice);
   }
   return choice;
+}
+
+function getComputerChoice() {
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  return computerChoice;
 }
 
 function playAgain() {
@@ -80,16 +83,19 @@ function playAgain() {
   }
 
   if (answer[0] === 'y') {
-    playerScore = 0;
-    computerScore = 0;
-    console.clear();
+    resetGameScore();
   }
+}
+
+function resetGameScore() {
+  playerScore = 0;
+  computerScore = 0;
+  console.clear();
 }
 
 while ((playerScore < WINNING_SCORE) && (computerScore < WINNING_SCORE)) {
   let choice = getChoice();
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
+  let computerChoice = getComputerChoice();
 
   calculateScore(choice, computerChoice);
   displayWinner(choice, computerChoice);
