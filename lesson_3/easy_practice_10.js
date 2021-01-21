2c634d7363c27d7a02c30fa096c4d31efd1933a7
Conor Chung <ConorChung@gmail.com>
2021-01-21 11:50:43 -0500

    finished easy practice 10

---
+++ lesson_3/easy_practice_10.js
@@ -0,0 +1,5 @@
+let advice = "Few things in life are as important as house training your pet dinosaur.";
+
+// Expected return value:
+// => 'Few things in life are as important as '
+console.log(advice.slice(0, (advice.indexOf('house'))));
