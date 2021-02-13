c613e206c1aab7a894724e527368fba835483d53
Conor Chung <ConorChung@gmail.com>
2021-02-12 16:39:57 -0800

    pedac practice

---
+++ small_problems/easy2_3.js
@@ -0,0 +1,19 @@
+// Problem
+// - Create a function that takes two arguements
+// - Multiply them together
+// - return the result
+// Input:
+// - two arguments (numbers)
+// Output:
+// - result of multiplying them together
+// Example/ Test Cases
+console.log(multiply(5, 3) === 15); // logs true
+
+// Data Structure
+// Algorithm
+// Code
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
